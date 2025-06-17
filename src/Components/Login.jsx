e191dcc6a075f12e8e68540e0ab89239e236fa89
Deepak Sharma <tjob674@gmail.com>
2025-06-17 14:51:54 +0530

    log in middle

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -39,9 +39,9 @@ const Login = () => {
 
     return (
         <div
-            className='flex items-center w-[90vw] md:w-2/3 justify-center'
+            className='flex items-center  min-h-screen justify-center'
         >
-            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+            <div className={`mx-auto w-[90vw] md:w-2/3 max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
                 <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo />
